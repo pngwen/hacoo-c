34428134c54c4cf373da05ac00e0603d18ebdee1
Robert Lowe <rlowe8@utm.edu>
2025-07-02 10:53:08 -0500

    gram product and lambda fixes

--- cpd.c
+++ cpd.c
@@ -10,8 +10,8 @@
 static void add_diagonal(matrix_t *matrix, double value);
 static void gram_product(matrix_t *res, matrix_t **factor, unsigned int modes, unsigned int mode);
 static cpd_result_t *cpd_alloc(struct hacoo_tensor *t, unsigned int rank);
-static double normalize_column(matrix_t *m, unsigned int col_idx);
-static void scale_factor_mode(cpd_result_t *result, unsigned int m);
+static double normalize_column(matrix_t *m, unsigned int col_idx, unsigned int iter);
+static void scale_factor_mode(cpd_result_t *result, unsigned int m, unsigned int iter);
 
 
 static void add_diagonal(matrix_t *matrix, double value) {
@@ -28,12 +28,20 @@ static void gram_product(matrix_t *res, matrix_t **factor, unsigned int modes, u
     // start with the identity matrix
     fill_identity_matrix(res);
 
-    for(int i=modes-1; i>=0; i--)
+    for(int n=0; n<modes; n++)
     {
         // skip the current mode
-        if(i==mode) continue;
-        mul_transpose_matrix(g, factor[i], factor[i]);
-        mul_matrix(res, res, g);
+        if(n==mode) continue;
+        mul_transpose_matrix(g, factor[n], factor[n]);
+
+        // compute the hadamard product res .* g
+        for (unsigned int j = 0; j < res->rows; j++)
+        {
+            for (unsigned int k = 0; k < res->cols; k++)
+            {
+                res->vals[j][k] *= g->vals[j][k];
+            }
+        }
     }
 
     free_matrix(g);
@@ -55,23 +63,46 @@ static cpd_result_t *cpd_alloc(struct hacoo_tensor *t, unsigned int rank)
     // Allocate the lambda vector
     result->lambda = calloc(rank, sizeof(double));
     if (!result->lambda) { goto bad; }
-    for(int i = 0; i < rank; i++)
+    for(int i = 0; i < result->rank; i++)
     {
         result->lambda[i] = 1.0; // Initialize lambda to 1.0
     }
 
     // Initialize the random arrays
+    /*
     for (unsigned int i = 0; i < t->ndims; i++)
     {
         result->factors[i] = new_random_matrix(t->dims[i], rank, 0, 1);
         if (!result->factors[i]) { goto bad; }
+    }*/
 
-        for(unsigned int j = 0; j < rank; j++)
-        {
-            // Normalize the column and store the norm
-            normalize_column(result->factors[i], j);
-        }
-    }
+    /* Populate the factor matrices with the following values for
+     * testing:
+     * result->factors[0] = 0.691945        0.018378
+     *                      0.283824        0.419653
+     *
+     * result->factors[1] = 0.939179        0.509681
+     *                      0.548621        0.280958
+     *
+     * result->factors[2] = 0.552988        0.507097
+     *                      0.813003        0.293792
+     */
+    result->factors[0] = new_matrix(2, rank);
+    result->factors[0]->vals[0][0] = 0.691945; 
+    result->factors[0]->vals[0][1] = 0.018378;
+    result->factors[0]->vals[1][0] = 0.283824;
+    result->factors[0]->vals[1][1] = 0.419653;
+    result->factors[1] = new_matrix(2, rank);
+    result->factors[1]->vals[0][0] = 0.939179;
+    result->factors[1]->vals[0][1] = 0.509681;
+    result->factors[1]->vals[1][0] = 0.548621;
+    result->factors[1]->vals[1][1] = 0.280958;
+    result->factors[2] = new_matrix(2, rank);
+    result->factors[2]->vals[0][0] = 0.552988;
+    result->factors[2]->vals[0][1] = 0.507097;
+    result->factors[2]->vals[1][0] = 0.813003;
+    result->factors[2]->vals[1][1] = 0.293792;
+
 
     return result;
 
@@ -82,15 +113,29 @@ bad:
 
 
 /* Normalize a column of the matrix and return its l2 norm.*/
-static double normalize_column(matrix_t *m, unsigned int col_idx)
+static double normalize_column(matrix_t *m, unsigned int col_idx, unsigned int iter)
 {
     // compute the L2 norm
     double norm = 0.0;
-    for (unsigned int i = 0; i < m->rows; i++)
-    {
-        norm += m->vals[i][col_idx] * m->vals[i][col_idx];
-    } 
-    norm = sqrt(norm);
+
+    if(iter == 0) {
+        // compute the L2 norm of the column
+        for (unsigned int i = 0; i < m->rows; i++)
+        {
+            norm += m->vals[i][col_idx] * m->vals[i][col_idx];
+        } 
+        norm = sqrt(norm);
+    } else {
+        // compute the max-norm of the column
+        norm = 1.0;
+        for (unsigned int i = 0; i < m->rows; i++)
+        {
+            if (fabs(m->vals[i][col_idx]) > norm)
+            {
+                norm = fabs(m->vals[i][col_idx]);
+            }
+        }
+    }
 
     // normalize the column
     for (unsigned int i = 0; i < m->rows; i++)
@@ -101,12 +146,13 @@ static double normalize_column(matrix_t *m, unsigned int col_idx)
 }
 
 
-static void scale_factor_mode(cpd_result_t *result, unsigned int m)
+static void scale_factor_mode(cpd_result_t *result, unsigned int m, unsigned int iter)
 {
     for(unsigned int j=0; j<result->rank; j++){
-        result->lambda[j] *= normalize_column(result->factors[m], j);
+        result->lambda[j] = normalize_column(result->factors[m], j, iter);
     }
 }
+
 
 
 // compute the canonical polyadic decomposition of a tensor
@@ -129,12 +175,12 @@ cpd_result_t *cpd(struct hacoo_tensor *t, unsigned int rank, unsigned int max_it
 
             // Compute the gram product and its inverse
             gram_product(gram, result->factors, t->ndims, mode);
-            add_diagonal(gram, GRAMREG);
+            //add_diagonal(gram, GRAMREG);
             invert_matrix(grami, gram);
 
             // Update the factor matrix
             mul_matrix(result->factors[mode], mttkrp_result, grami);
-            scale_factor_mode(result, mode);
+            scale_factor_mode(result, mode, iter);
 
 //-- DEBUGGING
 printf("Iter %u, mode %u: mttkrp_result norm = %f\n", iter, mode, matrix_frobenius_norm(mttkrp_result));
@@ -170,4 +216,29 @@ void cpd_result_free(cpd_result_t *result)
         free(result->lambda);
     }
     free(result);
+}
+
+
+void gram_test()
+{
+    matrix_t *a = new_matrix(2,2);
+    matrix_t *g = new_matrix(2,2);
+
+    /* populate a with:
+       0.9027    0.4909
+       0.9448    0.4893
+     */
+    a->vals[0][0] = 0.9027; a->vals[0][1] = 0.4909;
+    a->vals[1][0] = 0.9448; a->vals[1][1] = 0.4893;
+
+    printf("Iteration 1 Normalization:\n");
+    copy_matrix_to(g,a);
+    printf("lambda[0]=%f\n", normalize_column(g, 0, 0));
+    printf("lambda[1]=%f\n", normalize_column(g, 1, 0));
+    print_matrix(g);
+    
+    printf("Iteration 2 Normalization:\n");
+    printf("lambda[0]=%f\n", normalize_column(a, 0, 1));
+    printf("lambda[1]=%f\n", normalize_column(a, 1, 1));
+    print_matrix(a);
 }
