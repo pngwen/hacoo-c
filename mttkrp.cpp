e7af5966afc7f564900b137acc3536bdcb0efcb8
MeiLi Charles <gymmyp1@gmail.com>
2026-03-09 16:07:16 -0400

    revert mttkrp to aligned-malloc ver

--- mttkrp.cpp
+++ mttkrp.cpp
@@ -25,7 +25,7 @@ Returns:
 #include <cblas.h>
 
 /* Parallel MTTKRP */
-matrix_t *hacoo_mttkrp(struct hacoo_tensor *h, matrix_t **u, unsigned int n)
+matrix_t *mttkrp(struct hacoo_tensor *h, matrix_t **u, unsigned int n)
 {
     unsigned int fmax = u[0]->cols;
 
@@ -33,12 +33,18 @@ matrix_t *hacoo_mttkrp(struct hacoo_tensor *h, matrix_t **u, unsigned int n)
     matrix_t *res = new_matrix(h->dims[n], fmax);
 
     int num_threads = omp_get_max_threads();
-    matrix_t **partials = (matrix_t **) malloc(num_threads * sizeof(matrix_t *));
+
+    matrix_t **partials = (matrix_t **) MALLOC(num_threads * sizeof(matrix_t *));
 
     #pragma omp parallel
     {
         int tid = omp_get_thread_num();
         int nthreads = omp_get_num_threads();
+        int nnz_counter = 0;
+
+        //if (tid == 0) {
+            //printf("Number of threads: %d\n", nthreads);
+        //}
 
         partials[tid] = new_matrix(h->dims[n], fmax);
         matrix_t *local_res = partials[tid];
@@ -48,35 +54,48 @@ matrix_t *hacoo_mttkrp(struct hacoo_tensor *h, matrix_t **u, unsigned int n)
         int end = (start + chunk > h->nbuckets) ? h->nbuckets : start + chunk;
 
         unsigned int *idx = (unsigned int *) malloc(h->ndims * sizeof(unsigned int));
+        double *rank_vec = (double *) malloc(fmax * sizeof(double));
 
+        // Loop over assigned bucket vectors
         for (int i = start; i < end; i++) {
             bucket_vector *vec = &h->buckets[i];
-            if (vec->size == 0) continue;
+            if (vec->size == 0)
+                continue;
 
             for (size_t j = 0; j < vec->size; j++) {
+                nnz_counter++;
                 struct hacoo_bucket *cur = &vec->data[j];
 
-                // Unpack indices from ALTO encoding
+                // Get full index array from compressed HaCOO format
+
+                // Get full index array from compressed ALTO format
                 alto_unpack(cur->alto_idx, h->mode_masks, h->ndims, idx);
 
-                double *out = local_res->vals[idx[n]];
-
+                // Initialize rank vector with cur->value
                 for (int f = 0; f < fmax; f++) {
-                    double prod = cur->value;
-
-                    for (int d = 0; d < h->ndims; d++) {
-                        if (d == n) continue;
-                        prod *= u[d]->vals[idx[d]][f];
-                    }
-
-                    out[f] += prod;
+                    rank_vec[f] = cur->value;
                 }
+
+                // Multiply by the appropriate row from each factor matrix, skipping mode n
+                for (int d = 0; d < h->ndims; d++) {
+                    if (d == n) continue;
+                    double *vec_d = u[d]->vals[idx[d]];
+                    for (int f = 0; f < fmax; f++) {
+                        rank_vec[f] *= vec_d[f];
+                    }
+                }
+
+                // Accumulate into the local result row using daxpy
+                cblas_daxpy(fmax, 1.0, rank_vec, 1, local_res->data + idx[n] * fmax, 1);
             }
         }
+
+        free(rank_vec); // Free thread-local buffer
         free(idx);
     }
 
-    // Merge thread-local results into global matrix
+    // Merge all thread-local results into the global result
+    /* Parallel over threads */
     #pragma omp parallel
     {
         int tid = omp_get_thread_num();
@@ -85,8 +104,10 @@ matrix_t *hacoo_mttkrp(struct hacoo_tensor *h, matrix_t **u, unsigned int n)
         int end = (start + chunk > h->dims[n]) ? h->dims[n] : start + chunk;
 
         for (int i = start; i < end; i++) {
-            for (int t = 0; t < num_threads; t++) {
-                cblas_daxpy(fmax, 1.0, partials[t]->vals[i], 1, res->vals[i], 1);
+            for (int f = 0; f < fmax; f++) {
+                for (int t = 0; t < num_threads; t++) {
+                    res->vals[i][f] += partials[t]->vals[i][f];
+                }
             }
         }
     }
@@ -94,7 +115,8 @@ matrix_t *hacoo_mttkrp(struct hacoo_tensor *h, matrix_t **u, unsigned int n)
     for (int t = 0; t < num_threads; t++) {
         free_matrix(partials[t]);
     }
-    free(partials);
+
+    FREE(partials);
 
     return res;
 }
